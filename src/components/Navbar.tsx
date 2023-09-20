8654c9b971d94b2231f0bd9e515596be9281d379
mubasharazad12 <mubasharazad3@gmail.com>
2023-09-21 03:05:17 +0500

    initiated layout with tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,18 @@
 const Navbar = () => {
   return ( 
     <>
-      <h1 className="text-center text-9xl font-bold ">Hellosdad</h1>
+      <header className="flex justify-center items-center m-5">
+        
+        <div className="flex-1 w-5">
+          <h1>Heading</h1>
+        </div>
+        <nav className="flex-1 w-32">
+          <ul className="flex justify-center items-center">
+            <li> <a href="#">Home</a> </li>
+            <li> <a href="#">contact</a> </li>
+          </ul>
+        </nav>
+      </header>
     </>
    );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+
+const Sidebar = () => {
+    return ( 
+        <>
+            <aside className="flex">
+                <div className="flex-initial w-32">
+                    <img width={200} src="https://cdn.pixabay.com/photo/2023/06/10/14/33/real-estate-8054148_1280.png" alt="" />
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+        </>
+     );
+}
+ 
+export default Sidebar;
