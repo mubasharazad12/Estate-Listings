d7df508e0bd803a7dc7eb3ed81e229649dda86e6
mubasharazad12 <mubasharazad3@gmail.com>
2023-09-27 03:29:15 +0500

    Adding box icons ts and tables in jsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -13,33 +13,39 @@ const Homepage = () => {
                 </div>
             </section>
 
-            <section>
-            <table className="table-auto">
-                <thead>
-                    <tr>
-                    <th>Song</th>
-                    <th>Artist</th>
-                    <th>Year</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    <td>Malcolm Lockyer</td>
-                    <td>1961</td>
-                    </tr>
-                    <tr>
-                    <td>Witchy Woman</td>
-                    <td>The Eagles</td>
-                    <td>1972</td>
-                    </tr>
-                    <tr>
-                    <td>Shining Star</td>
-                    <td>Earth, Wind, and Fire</td>
-                    <td>1975</td>
-                    </tr>
-                </tbody>
-            </table>
+            <section className="flex px-5">
+                <table className="table-fixed border-collapse border w-full">
+                <caption className="caption-top text-left p-5">
+                    Table heading
+                </caption>
+                    <thead>
+                        <tr>
+                        <th className="px-5 py-3 border-b-2 border-gray-200 text-left bg-gray-100  text-xs font-semibold text-gray-600 uppercase tracking-wider" >Song</th>
+                        <th className="px-5 py-3 border-b-2 border-gray-200 text-left bg-gray-100  text-xs font-semibold text-gray-600 uppercase tracking-wider"  >Artist</th>
+                        <th  className="px-5 py-3 border-b-2 border-gray-200 text-left bg-gray-100  text-xs font-semibold text-gray-600 uppercase tracking-wider" >Year</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">Malcolm Lockyer</td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">1961</td>
+                        </tr>
+                        <tr>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">Witchy Woman</td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">The Eagles</td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">1972</td>
+                        </tr>
+                        <tr>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">Shining Star</td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">Earth, Wind, and Fire</td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">1975</td>
+                        </tr>
+                    </tbody>
+                    <caption className="caption-bottom px-5 py-5 border-b border-gray-200">
+                        Table captioon @ if require or pagination
+                    </caption> 
+                </table>
             </section>
         </>
      );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+import * as React from 'react';
+declare module 'boxicons';
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+        'box-icon': any;
+  
+     
+      // ... hundreds more
+    }
+  }
+}
