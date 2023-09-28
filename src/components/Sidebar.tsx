3cf0282ede34ddba6f23d0eedd50aff69d9ce436
mubasharazad12 <mubasharazad3@gmail.com>
2023-09-28 21:36:10 +0500

    tailwind config edits

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,37 +2,48 @@ import logo from '../assets/Logo.png'
 import 'boxicons'
 
 const Sidebar = () => {
+
+
     return ( 
         <>
             <aside className="flex bg-gray-100 w-20 sm:w-56 min-h-screen text-black shadow-2xl">
                
                 <nav className='truncate'>
-                    <div className="flex items-center w-20 sm:w-36 flex-col sm:flex-row sm:m-10 mt-10">   
+                    <div className="flex items-center w-20 sm:w-36 flex-col sm:flex-row sm:m-10 mt-10 bg-ne" >   
                         <img width={200} src={logo}alt="" />
                         {/* <span>logo name</span> */}
-                       
+                      
                     </div>
+                  
+    
                     <hr className="w-20  sm:w-56 h-[1px] bg-gray-200 border-none mt-10 "/>
-                    <ul className="ml-20 mt-16 space-y-16 text-purple-800  ">
-                        <li className="">
-                            <a href="#">Home</a>
-                            <box-icon type='solid' name='balloon'></box-icon>
-                            <abbr></abbr>
-                        </li>
+                    <ul className="mt-16 sm:ml-16 ml-8 space-y-16 text-gray-700 font-display font-semibold ">
                         <li>
-                            <a href="#">Home</a>
+                            <a className="flex hover:fill-teal-700 ease-in-out duration-300 text-primary" href="#"> 
+                                <span className='pr-1 '> <box-icon type='solid' name='dashboard'></box-icon></span> 
+                                <span className='hidden sm:block'>Dashboard</span>  
+                            </a>
                         </li>
-                        <li>
-                            <a href="#">Home</a>
+                        <li className="flex">
+                            <span className='pr-1'> <box-icon  name='objects-vertical-bottom' type='solid' ></box-icon></span>
+                            <a href="#">Order List</a>
                         </li>
-                        <li>
-                            <a href="#">Home</a>
+                        <li className="flex">
+                            <span className='pr-1'> <box-icon name='buildings' type='solid' ></box-icon></span>    
+                            <a href="#">Property List</a>
+                        </li >
+                        <li  className="flex">
+                            <span className='pr-1'> <box-icon  name='user-detail' type='solid' ></box-icon></span>
+                            <a href="#">Customer List</a>
                         </li>
-                        <li>
-                            <a href="#">Home</a>
+                        <li  className="flex">
+                            <span className='pr-1'> <box-icon name='like' type='solid' ></box-icon></span>
+                            <a href="#">Reviews</a>
                         </li>
-                        <li className="absolute bottom-0 pb-10">
-                            <a href="#">logout</a>
+                        <li className="absolute bottom-0 pb-10 flex">
+            
+                            <span className='pr-1'> <box-icon type='solid' name='dashboard'></box-icon></span>
+                            <a className='hidden sm:block' href="#">Logout</a>
                         </li>
                         
                     </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
   content: [
     "./index.html",
@@ -6,6 +8,14 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      display: ["Onest", "sans-serif"],
+    },
+    colors:{
+      primary: {...colors.sky , DEFAULT:colors.sky[600]},
+      secondary: {...colors.blue , DEFAULT:colors.blue[600]},
+      neutral:  colors ,
+    },
   },
   plugins: [],
 }
